23303196366478b0c2e0bc40986bd9bd91890ed8
Dilan Li <dilanli0706@gmail.com>
2020-06-04 16:52:52 -0600

    first draft

---
+++ script.js
@@ -0,0 +1,85 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+
+
+//this function generates a random password based on user's selected criteria
+function generatePassword() {
+
+  //user defines password length
+  var passwordLength = prompt("Please choose a length in between 8-128 characters");
+  if (passwordLength > 128 || passwordLength < 8) {
+      alert("The password length needs to be at least 8 characters long and shorter than 128 characters")
+      passwordLength = prompt("Please choose a length in between 8-128 characters");
+  }
+
+  var possibleAnswers = "";
+
+  //user decides whether to include lowercase characters
+  var lowercase = confirm("Would you like to include lowercase characters in your password?");
+  if (lowercase) {
+    possibleAnswers += "abcdefghijklmnopqrstuvwxxyz";
+  } 
+
+  //user decides whether to include uppercase characters
+  var uppercase = confirm("Would you like to include uppercase characters in your password?");
+  if (uppercase) {
+    possibleAnswers += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
+
+  //user decides whether to include numbers in password
+  var numbers = confirm("Would you like to include numbers in your password?");
+  if (numbers) {
+    possibleAnswers += 1234567890;
+  }
+
+  //user decides whether to include special characters in password
+  var specialCharacters = confirm("Would you like to include special characters in your password?");
+  if (specialCharacters) {
+    possibleAnswers += "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+  }
+
+
+  // check if user chose at least one character type
+  while (possibleAnswers === ""){
+  alert("You must include at least one character type");
+  var lowercase = confirm("Would you like to include lowercase characters in your password?");
+  if (lowercase) {
+    possibleAnswers += "abcdefghijklmnopqrstuvwxxyz";
+  } 
+  var uppercase = confirm("Would you like to include uppercase characters in your password?");
+  if (uppercase) {
+    possibleAnswers += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
+  var numbers = confirm("Would you like to include numbers in your password?");
+  if (numbers) {
+    possibleAnswers += 1234567890;
+  }
+  var specialCharacters = confirm("Would you like to include special characters in your password?");
+  if (specialCharacters) {
+    possibleAnswers += "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+  }
+  }
+
+  //generate random password at the length user specified in the beginning
+  var newPassword = "";
+
+  for (var i = 1; i <= passwordLength; i++) {
+    newPassword += possibleAnswers.charAt(Math.floor(Math.random() * possibleAnswers.length));
+  }
+  
+  return newPassword;
+}
