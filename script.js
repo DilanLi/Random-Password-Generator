bd6c94d711a13a3836bc61926729d591390d1dde
Dilan Li <dilanli0706@gmail.com>
2020-06-04 22:37:06 -0600

    fixed loop when user deny all character types

--- script.js
+++ script.js
@@ -21,12 +21,12 @@ function generatePassword() {
 
   //user defines password length
   var passwordLength = prompt("Please choose a length in between 8-128 characters");
+  var possibleAnswers = "";
+  
   if (passwordLength > 128 || passwordLength < 8) {
       alert("The password length needs to be at least 8 characters long and shorter than 128 characters")
       passwordLength = prompt("Please choose a length in between 8-128 characters");
   }
-
-  var possibleAnswers = "";
 
   //user decides whether to include lowercase characters
   var lowercase = confirm("Would you like to include lowercase characters in your password?");
@@ -52,26 +52,10 @@ function generatePassword() {
     possibleAnswers += "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
   }
 
-
-  // check if user chose at least one character type
-  while (possibleAnswers === ""){
+  //check if user chose at least one character type
+  if (possibleAnswers === ""){
   alert("You must include at least one character type");
-  var lowercase = confirm("Would you like to include lowercase characters in your password?");
-  if (lowercase) {
-    possibleAnswers += "abcdefghijklmnopqrstuvwxxyz";
-  } 
-  var uppercase = confirm("Would you like to include uppercase characters in your password?");
-  if (uppercase) {
-    possibleAnswers += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  }
-  var numbers = confirm("Would you like to include numbers in your password?");
-  if (numbers) {
-    possibleAnswers += 1234567890;
-  }
-  var specialCharacters = confirm("Would you like to include special characters in your password?");
-  if (specialCharacters) {
-    possibleAnswers += "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
-  }
+  return generatePassword();
   }
 
   //generate random password at the length user specified in the beginning
